487c7a2a39e113238ba6228b50cdbbd6263333f0
Karan0723 <kr217040@gmail.com>
2023-09-13 19:53:39 +0530

    Update website content

--- data.js
+++ data.js
@@ -11,18 +11,6 @@ let songs = [
         artist: 'Daniel Levi',
         cover: 'images/on.jpg'
     },
-    {
-        name: 'Shri Ram Janki',
-        path: 'musics/ram bethe hain.mp3',
-        artist: 'Ram Kumar Lakha',
-        cover: 'images/ram bethe hain.jpg'
-    },
-    {
-        name: 'Bhola Bhandari',
-        path: 'musics/bhola bhandari.mp3',
-        artist: 'Hanshraj Raghuwanshi',
-        cover: 'images/bhola hai bhandari.jpg '
-    },
 
     {
         name: ' Ram Siya Ram',
@@ -36,24 +24,9 @@ let songs = [
         artist: 'Kamal Kahlon and Param Singh',
         cover: 'images/daru badnam.jpg'
     },
-    {
-        name: 'Ban Meri Raani',
-        path: 'musics/ban ja tu raani.mp3',
-        artist: 'Guru Randhawa',
-        cover: 'images/ban ja tu rani.jpg'
-    },
-    {
-        name: 'Baapu Zimedar',
-        path: 'musics/bapu-zimedar.mp3',
-        artist: 'Jassi Gill',
-        cover: 'images/cover 4.png'
-    },
-  
-    {
-        name: 'Are Dwarpalo',
-        path: 'musics/dwarpalo.mp3',
-        artist: 'Swasti Mehul',
-        cover: 'images/dwarpalo.jpg'
-    },
+
+
+
+
 ]
 
